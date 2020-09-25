c966cc8ada76720a22a7b32ce72721bcdee149ce
mac <eamil@qq.com>
2020-09-25 18:04:07 +0800

    first commit

---
+++ GCDFetchFeed/SMNetManager.h
@@ -0,0 +1,30 @@
+//
+//  SMNetManager.h
+//  GCDFetchFeed
+//
+//  Created by DaiMing on 16/1/21.
+//  Copyright © 2016年 Starming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <ReactiveCocoa/RACEXTScope.h>
+
+typedef void(^SuccessBlock) (NSDictionary * result);
+typedef void(^FailureBlock) (NSError * error);
+
+
+
+@interface SMNetManager : AFHTTPSessionManager
+
++ (SMNetManager *)shareInstance;
+
+@property (nonatomic, strong) NSMutableArray *feeds;
+@property (nonatomic, strong) NSMutableDictionary *icons;
+
+- (RACSignal *)fetchAllFeedWithModelArray:(NSArray *)modelArray;
+
++ (BOOL)isWifi;
+
+@end
---
+++ Pods/Target Support Files/Pods-GCDFetchFeed/Pods-GCDFetchFeed-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GCDFetchFeed : NSObject
+@end
+@implementation PodsDummy_Pods_GCDFetchFeed
+@end
